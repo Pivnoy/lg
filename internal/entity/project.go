b0c5be606260b0d5a03ec6bdc7e398fabbee1ae8
romeros69 <jefops@gmail.com>
2022-10-31 22:57:46 +0300

    feat: add postman collection for projects endpoints

--- internal/entity/project.go
+++ internal/entity/project.go
@@ -1,10 +1,13 @@
 package entity
 
+import "github.com/gofrs/uuid"
+
 type Project struct {
-	ID               int64  `json:"id"`
-	Name             string `json:"name"`
-	Description      string `json:"description"`
-	ProjectLink      string `json:"link"`
-	PresentationLink string `json:"presentation"`
-	CreatorID        int64  `json:"creator_id"`
+	ID               int64     `json:"id"`
+	UUID             uuid.UUID `json:"uuid"`
+	Name             string    `json:"name"`
+	Description      string    `json:"description"`
+	ProjectLink      string    `json:"link"`
+	PresentationLink string    `json:"presentation"`
+	CreatorID        int64     `json:"creator_id"`
 }
--- migrations/init.sql
+++ migrations/init.sql
@@ -1,5 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 create table project (
     id serial primary key,
+    uuid uuid DEFAULT uuid_generate_v4(),
     name varchar(255) not null,
     description text not null,
     project_link varchar(255),
