6922bb1632b257ab14a3fbbb35d9ac1d5877dc0c
Evgenij Makarev <evgenij_makarev20021@mail.ru>
2022-11-03 02:11:15 +0300

    user uuid

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,7 +1,10 @@
 package entity
 
+import "github.com/google/uuid"
+
 type User struct {
-	ID       int64  `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID       int64     `json:"id"`
+	UUID     uuid.UUID `json:"uuid"`
+	Email    string    `json:"email"`
+	Password string    `json:"password"`
 }
